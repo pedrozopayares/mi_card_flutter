75c5f07e767b54177854e7c99124b36aeb6b9dfe
Javier Andres Pedrozo Payares <pedrozopayares@gmail.com>
2020-11-03 22:14:10 -0500

    Column VeticalDirection exercise.

--- lib/main.dart
+++ lib/main.dart
@@ -13,13 +13,28 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Container(
-            height: 100.0,
-            width: 100.0,
-            margin: EdgeInsets.only(left: 30.0),
-            padding: EdgeInsets.all(20.0),
-            color: Colors.white,
-            child: Text("Hello"),
+          child: Column(
+            verticalDirection: VerticalDirection.down,
+            children: [
+              Container(
+                height: 100.0,
+                width: 100.0,
+                color: Colors.white,
+                child: Text("Hello"),
+              ),
+              Container(
+                height: 100.0,
+                width: 100.0,
+                color: Colors.blue,
+                child: Text("Hello"),
+              ),
+              Container(
+                height: 100.0,
+                width: 100.0,
+                color: Colors.red,
+                child: Text("Hello"),
+              ),
+            ],
           ),
         ),
       ),
